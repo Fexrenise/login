8ba47e8072d93fab8c9ed605531aebb5d3e9fd89
Fexrenise <bayramovafexrenise@gmail.com>
2022-03-26 12:06:40 +0400

    first commit

---
+++ login.js
@@ -0,0 +1,25 @@
+function FullName() {
+    let gmail = "Fdevelopers46@gmail.com";
+    let password = "Nopassword2022#";
+    let YourGmail = document.getElementById('inp').value;
+    let YourPassword = document.getElementById('inf').value;
+    if (gmail == YourGmail && password == YourPassword) {
+        alert('Kod və Gmail Düzdür!');
+    }else if (gmail !== YourGmail && password == YourPassword) {
+        document.getElementById('mail').innerHTML="Gmail Yanlışdır"
+    }else if (gmail == YourGmail && password !== YourPassword) {
+        document.getElementById('pass').innerHTML="Kod Yanlışdır"
+    }else{
+        alert('Kod və Gmail Yanlışdır!');
+    };
+}
+
+function Word(){
+    let show =document.getElementById('')
+    if (show.type =='password'){
+        show.type='text';
+    }
+    else{
+        show.type='password';
+    }
+}
